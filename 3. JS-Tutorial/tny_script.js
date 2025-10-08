707b26b06477239b3371712da02cd2bb2376a73b
jacquesvSWDV <jacques.valentine@rdpolytech.ca>
2025-10-08 11:19:03 -0600

    update git push origin master

--- 3. JS-Tutorial/tny_script.js
+++ 3. JS-Tutorial/tny_script.js
@@ -9,3 +9,14 @@
 
 */
 
+//Display current date & time
+document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+
+//Display the time left until New Years Eve Bash
+document.getElementById('days').textContent = "dd";
+document.getElementById('hrs').textContent = "hh";
+document.getElementById('mins').textContent = "mm";
+document.getElementById('secs').textContent = "ss";
+
+//Store the current date and time
+var currentDate = new Date();
